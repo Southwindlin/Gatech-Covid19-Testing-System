99fc2329bbbfec2b2e3cba443a57763e98241846
huskychan <homyvchan@users.noreply.github.com>
2020-11-22 23:14:08 -0500

    finish S9,S12-S15 S16
    S16a still further discussion

--- FlaskDemo/app.py
+++ FlaskDemo/app.py
@@ -251,6 +251,127 @@ def viewAppointment():
 
 
 
+@app.route('/viewTester',methods=['GET'])
+def viewTester():
+
+    cursor = mysql.connection.cursor()
+
+    try:
+        cursor.callproc("view_testers")
+    except pymysql.IntegrityError or KeyError as e:
+        return "unable to view because " + str(e)
+    else:
+        #print the view to the html
+
+        # select from the student_view_results_result
+        sql = "select * from view_testers_result"
+        cursor.execute(sql)
+        mysql.connection.commit()
+        content = cursor.fetchall()
+
+        # get the field name
+        sql = "SHOW FIELDS FROM view_testers_result"
+        cursor.execute(sql)
+        mysql.connection.commit()
+        labels = ['UserName','Name','Phone Number','Assigned Site']
+
+        #visualization template source:
+        #https://blog.csdn.net/a19990412/article/details/84955802
+
+        return render_template('viewTester.html', labels=labels, content=content)
+
+
+@app.route('/createTestSite',methods=['GET','POST'])
+def createTestSite():
+    if request.method =='GET':
+        return render_template('createTestSite.html')
+    elif request.method == 'POST':
+        cursor = mysql.connection.cursor()
+
+        site = request.form.get("Site")
+        address = request.form.get("Address")
+        city = request.form.get("City")
+        state = request.form.get("State")
+        zipCode = request.form.get("Zip")
+        location = request.form.get("Location")
+        tester = request.form.get("Tester")
+        try:
+            cursor.callproc("create_testing_site",[site,address,city,state,zipCode,location,tester])
+        except pymysql.IntegrityError or KeyError as e:
+            return "unable to create because " + str(e)
+        else:
+            mysql.connection.commit()
+            return "create_testing_site alreay"
+
+
+@app.route('/viewPools',methods=['GET','POST'])
+def viewPools():
+    if request.method =='GET':
+        return render_template('viewPools.html')
+    elif request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        startDate = None if request.form.get('DateStart') == '' else request.form.get('DateStart')
+        endDate = None if request.form.get('DateEnd') == '' else request.form.get('DateStart')
+        status = request.form.get("Status")
+        labtech = request.form.get('LabTech')
+
+
+        try:
+            cursor.callproc("view_pools",[startDate,endDate,labtech,status])
+        except pymysql.IntegrityError or KeyError as e:
+            return "unable to view because " + str(e)
+        else:
+            #print the view to the html
+
+            # select from the student_view_results_result
+            sql = "select * from view_pools_result"
+            cursor.execute(sql)
+            mysql.connection.commit()
+            content = cursor.fetchall()
+
+            # get the field name
+            sql = "SHOW FIELDS FROM view_pools_result"
+            cursor.execute(sql)
+            mysql.connection.commit()
+            labels = ['Pool ID','Test Ids','Date Processed','Processed By','Pool Status']
+
+            #visualization template source:
+            #https://blog.csdn.net/a19990412/article/details/84955802
+
+            return render_template('viewPools.html', labels=labels, content=content)
+
+
+
+@app.route('/poolResult',methods=['GET'])
+def poolMetaDate():
+    cursor = mysql.connection.cursor()
+
+    try:
+        cursor.callproc("pool_metadata")
+    except pymysql.IntegrityError or KeyError as e:
+        return "unable to view because " + str(e)
+    else:
+        #print the view to the html
+
+        # select from the student_view_results_result
+        sql = "select * from pool_metadata_result"
+        cursor.execute(sql)
+        mysql.connection.commit()
+        content = cursor.fetchall()
+
+        # get the field name
+        sql = "SHOW FIELDS FROM pool_metadata_result"
+        cursor.execute(sql)
+        mysql.connection.commit()
+        labels = ['Pool ID','Date Processed','Pooled Result','ProcessedBy']
+
+        #visualization template source:
+        #https://blog.csdn.net/a19990412/article/details/84955802
+
+        return render_template('poolResult.html', labels=labels, content=content)
+
+
+
 
 
 
